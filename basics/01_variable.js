d04b7e37fa1283ac7c1fd9cdef5c92946d883956
Muskan Tyagi <138864240+Muskantyagi2@users.noreply.github.com>
2025-12-28 17:00:42 +0000

    file names updated

---
+++ basics/01_variable.js
@@ -0,0 +1,19 @@
+/*
+for memeory management - variable and const 
+
+constant - const ( cant chnge value after defining)
+variable - var & let ( can change value )
+
+but not prefer var ( because of the problm of block scope and fucntion scope )
+and use let for it 
+
+*/
+
+const accountId = 123
+let accountEmail = "muskan@gmail.com"
+var accountPassword = "mypassword"
+accountName = "muskan"
+
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountName, accountState])
