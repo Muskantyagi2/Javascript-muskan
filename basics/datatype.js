7d4957abbf20926f820275aad0f6c7caaa928b95
Muskan Tyagi <138864240+Muskantyagi2@users.noreply.github.com>
2025-12-28 15:57:07 +0000

    datatype done

---
+++ basics/datatype.js
@@ -0,0 +1,36 @@
+"use strict"   //treat all js code as new version
+
+// alert ( 3 + 3)     show error in node.js but it will work fine in browser
+//READILIBITY  CODE is imp
+
+//documentation - MDN    &   tc39.es  ( ECMA ) for standard like for loop output .....
+
+
+/*
+data type - primitive data types :- 
+1. number  ( 2 ki power 53 ) 
+2. string  ( "" , '',  `` template)
+3. boolean  ( true, flase)
+4. null  ( standalone value - empty value )  // object type
+5. undefined  (  value not defined yet)
+6. symbol ( to find uniuqueness in react js)
+7. BigInt  ( to store large integers   in proejcts like reddit or trading etc..)
+
+non-primitive data types :-
+1. object
+*/
+
+
+//code
+console.log("number : ", 3)
+console.log("string : ", "hello")
+console.log("noolena : ", true)
+console.log("null: ",null)
+console.log("undefined : ", undefined)
+console.log("symbol : ", Symbol("id"))
+console.log("BigInt : ", 1234567890123456789012345678901234567890n)
+
+
+console.log(typeof null)   //object
+
+console.log(typeof undefined)   //undefined
