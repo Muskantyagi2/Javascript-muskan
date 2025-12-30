0b40eabf52691a75e26acaf1bd676eeec76b20a7
Muskan Tyagi <138864240+Muskantyagi2@users.noreply.github.com>
2025-12-30 17:42:28 +0000

    comparisons

---
+++ basics/04_comparison.js
@@ -0,0 +1,16 @@
+//comparison operator results always in true or false
+
+// console.log(2>4)
+// console.log(2<4)
+// console.log(2<=4)
+// console.log(2>=4)
+// console.log(2==4)
+// console.log(2!=4)
+
+
+console.log("2" == 2)  //true because == only check value not datatype 
+console.log("2" === 2)  //false beacuse === check vlue and datatype of both 
+
+console.log(null > 0)
+console.log(null == 0)
+console.log( null >= 0)
