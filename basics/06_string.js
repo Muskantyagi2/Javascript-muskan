8b1a1a632fd14646f617b3fb4aba675772e2cf51
Muskan Tyagi <138864240+Muskantyagi2@users.noreply.github.com>
2026-01-04 15:28:33 +0000

    string done

--- basics/06_string.js
+++ basics/06_string.js
@@ -16,7 +16,7 @@ let str3 = 'hell world'
 // console.log(str3[3])
 
 //2. using new keyword
-let user = new String ('muskan')
+let user = new String ('muskan tyagi')
 // console.log(user)
 
 
@@ -44,15 +44,47 @@ let user = new String ('muskan')
 // console.log(user.includes("an"))  // true - it will check if the string is present in the given string or not
 
 
-let para = "The dog is between two another dog"
-let searchTerm = "dog"
-let indexOfFirst = para.indexOf(searchTerm)
+// let para = "The dog is between two another dog"
+// let searchTerm = "dog"
+// let indexOfFirst = para.indexOf(searchTerm)
 
-console.log(`inex of first ${searchTerm} is ${indexOfFirst}`);
+// console.log(`inex of first ${searchTerm} is ${indexOfFirst}`);
 
-console.log(
-  `The index of the second "${searchTerm}" is ${para.indexOf(
-    searchTerm,
-    indexOfFirst + 1,
-  )}`,
-);
+// console.log(
+//   `The index of the second "${searchTerm}" is ${para.indexOf(
+//     searchTerm,
+//     indexOfFirst + 1,
+//   )}`,
+// );
+
+
+
+const anotherstring = user.substring(0,4)   //index will start from 0 - 4 ( -1)
+// console.log(anotherstring)
+
+const anotherslice = user.slice(-8,8)
+// console.log(anotherslice)
+
+
+const anotherstring2 = "      muskan.     "
+const trimstring = anotherstring2.trim()  //it will remove space from both sides ( can use when taking input from user)   //it works only on white saces and line terminators such as /n, /r
+// console.log(trimstring)
+const trimstart = anotherstring2.trimStart()   
+const trimend = anotherstring2.trimEnd()
+// console.log(trimstart)
+// console.log(trimend)
+
+
+//replace method
+
+let url = "https://muskan@20tygai.com"
+console.log(url.replace('@20', '-'))      //it will replace selected part with teh new part
+console.log(url.includes('tyagi'))
+
+
+//split method
+let message = "hey muskan taygi"
+
+console.log(message.split(' '))  //it will split the string at space and return array of strings
+console.log(message.split(' ', '2'))  // it will split the string at space and return array of strings but only 2 strings will be returned
+
